8296a0c88f158a2ba09ccbf9bb26c3c8c50f5f26
MO JO <mbeleckmbeleck@gmail.com>
2022-10-23 11:14:01 +0000

    basic express

---
+++ controllers/users.js
@@ -0,0 +1,67 @@
+import { v4 as userID } from "uuid";
+userID();
+
+let users = [
+	// {
+	// 	firstName: "John",
+	// 	lastName: "Doe",
+	// 	age: 25,
+	// },
+	// {
+	// 	firstName: "Jane",
+	// 	lastName: "Doe",
+	// 	age: 24,
+	// },
+];
+
+export const createUser = (req, res) => {
+	const user = req.body;
+	// const userId = userID();
+
+	// const userWithId = {
+	// 	...user,
+	// 	id: userID(),
+	// };
+	users.push({
+		...user,
+		id: userID(),
+	});
+	res.send(`User with the name ${user.firstName} added to the database`);
+};
+
+export const getUser = (req, res) => {
+	const { id } = req.params;
+
+	const foundUser = users.find((user) => user.id == id);
+	res.send(foundUser);
+};
+
+export const deleteUser = (req, res) => {
+	const { id } = req.params;
+
+	users = users.filter((user) => user.id != id);
+	res.send(`USer with the id ${id} deleted from the database`);
+};
+
+export const updateUser = (req, res) => {
+	const { id } = req.params;
+
+	const { firstName, lastName, age } = req.body;
+	const userUpdate = users.find((user) => user.id == id);
+
+	if (firstName) {
+		userUpdate.firstName = firstName;
+	}
+	if (lastName) {
+		userUpdate.lastName = lastName;
+	}
+	if (age) {
+		userUpdate.age = age;
+	}
+	res.send(`USer with the id ${id} has been updated`);
+};
+
+export const getUsers = (req, res) => {
+	console.log(users);
+	res.send(users);
+};
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+import express from "express";
+const router = express.Router();
+
+import {
+	createUser,
+	getUser,
+	deleteUser,
+	getUsers,
+	updateUser,
+} from "../controllers/users.js";
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+router.get("/:id", getUser);
+
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", updateUser);
+
+export default router;
